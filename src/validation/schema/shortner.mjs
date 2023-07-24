618603f69b359d0d69420740b2de6419cb3b31a4
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-07-24 16:40:54 +0100

    Create Url Shortener Schema Validation

---
+++ src/validation/schema/shortner.mjs
@@ -0,0 +1,11 @@
+// Shortener Schema Validation
+import joi from 'joi'
+
+const validUrl = joi.object({
+    userId: joi.string().regex(/^[a-fA-F0-9]{24}$/),
+    long_url: joi.string().required().trim(),
+    create_your_short_url: joi.string().required().trim(),
+})
+
+export { validUrl }
+
